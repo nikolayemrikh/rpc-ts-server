bfd1ef87d74fb1a3de9d08186d3e717f7cf65d13
Nikolay Emrikh <redishko@gmail.com>
2025-09-02 23:19:25 +0300

    fix

--- src/createRPCServer/types.ts
+++ src/createRPCServer/types.ts
@@ -1,4 +1,5 @@
-export type TMethod = (...args: unknown[]) => Promise<unknown>;
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type TMethod = (...args: any[]) => Promise<any>;
 
 export interface IMethods {
   [key: string]: TMethod | IMethods;
